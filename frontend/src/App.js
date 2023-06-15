3977bc600302652d1f0386eb690ae134c4278c5c
MD Kamran khan <mohammadkkhan78@gmail.com>
2023-06-15 12:17:13 +0530

    ss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 
 import Footer from './component/Footer';
+import Homepage from './component/Homepage';
 import { Navbar } from './component/Navbar';
 
 function App() {
   return (
     <div >
 <Navbar/>
+<Homepage/>
      <Footer/>
     </div>
   );
