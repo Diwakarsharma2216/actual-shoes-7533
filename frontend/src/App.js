85591dc859235e990bacb7b018b1e584c82fd73c
diwakar01 <diwakarsharma2216@gmail.com>
2023-06-18 11:47:28 +0530

    update singupPage & LoginPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Homepage from './component/Homepage';
 import { Navbar } from './component/Navbar';
 import Dropdown from "./component/Dropdown"
 import Navbar2 from './component/Navbar2';
+import SingUpPage from './component/SingUpPage';
+import LoginPage from './component/LoginPage';
 function App() {
   return (
     <div >
@@ -11,6 +13,9 @@ function App() {
 <Navbar2/>
 <Homepage/>
 <Footer/>
+
+{/*  My code Here ! */}
+{/* <LoginPage /> */}
     </div>
   );
 }
--- frontend/src/component/Singup.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Singup = () => {
-  return (
-    <div>Singup</div>
-  )
-}
