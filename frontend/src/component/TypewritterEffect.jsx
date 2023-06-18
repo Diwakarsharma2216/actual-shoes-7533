09e4e4b574a87bd79e99dc4a1026fe7c7980e512
diwakar01 <diwakarsharma2216@gmail.com>
2023-06-18 20:53:37 +0530

    .

--- frontend/src/component/TypewritterEffect.jsx
+++ frontend/src/component/TypewritterEffect.jsx
@@ -16,7 +16,7 @@ const TypewriterEffect = ({ data }) => {
     }
   }, [currentIndex, data]);
 
-  return <Text fontSize={{base:"21px",md:"30px"}} color='white'>
+  return <Text fontSize={{base:"21px",md:"30px"}} color='cyan'>
   {text}
 </Text>
 };
